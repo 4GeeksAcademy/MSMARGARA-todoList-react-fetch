0230dbfa9ec8a4814a34cfed860951a75f8140a8
MSMARGARA <142249109+MSMARGARA@users.noreply.github.com>
2024-01-19 21:14:18 +0000

    DELETE

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,22 +13,23 @@ const Home = () => {
           "https://playground.4geeks.com/apis/fake/todos/user/msmargara"
         );
 
-        if (!response.ok) {
-          throw new Error("Error fetching data");
-        }
+        if (!response.ok) throw new Error("Error fetching data");
 
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Error:", error);
-      }
+      const result = await response.json();
+      console.log(result);
+      const undoneTasks = result.filter(task => !task.done);
+      setData(undoneTasks);
+      setInputValue("");
+  } catch (error) {
+    console.error("Error:", error);
+  }
     };
 
     fetchData();
     const today = new Date();
     const formattedDate = today.toLocaleDateString("es-AR"); 
     setCurrentDate(formattedDate);
-  }, []);
+  }, []);  
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -78,34 +79,37 @@ const Home = () => {
   };
 
   const deleteTodo = async (taskId) => {
-    try {
-      const response = await fetch(
-        `https://playground.4geeks.com/apis/fake/todos/user/msmargara/${taskId}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
+      try {
+        const updatedData = data.map((task) =>
+        task.id === taskId ? { ...task, done: true } : task
+        );;
+    
+        const response = await fetch(
+          "https://playground.4geeks.com/apis/fake/todos/user/msmargara",
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedData),
+          }
+        );
+    
+        if (!response.ok) {
+          throw new Error("Error updating task");
         }
-      );
-
-      if (!response.ok) {
-        throw new Error("Error deleting task");
+    
+        const result = await response.json();
+        console.log("Response from PUT:", result);
+    
+        const undoneTasks = updatedData.filter((task) => !task.done);
+    
+        setData(undoneTasks);
+      } catch (error) {
+        console.error("Error updating task:", error);
       }
-
-      const result = await response.json();
-      console.log("Response from DELETE:", result);
-
-      if (result.result === "ok") {
-        setData((prevData) => prevData.filter((task) => task.id !== taskId));
-      } else {
-        console.log("Error al borrar la tarea:", result);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
   };
-
+  
   return (
     <div className="container px-5 py-3">
       <div className="d-flex justify-content-between align-items-end">
@@ -137,7 +141,7 @@ const Home = () => {
           >
 			<div className="d-flex justify-content-between">
 				<div>
-					<span>{task.label}</span>
+        ✓ <span>{task.label}</span>
 				</div>
 				<div>
 				{hoverItem === task.id && (
